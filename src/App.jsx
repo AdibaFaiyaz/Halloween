2d1eae932906737f4be1f94962534d73a7f19678
adibafaiyaz16@gmail.com <adibafaiyaz16@gmail.com>
2023-10-24 01:47:34 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
@@ -16,25 +16,28 @@ function App() {
         </ul>
       </nav>
       <main>
-        <div className='main bg-gray-900  flex justify-center'>
-          <div className='text-3xl py-10 text-white font-bold xl:text-4xl'>Spooky Season is Here!</div>
-        </div>
-        <div className='main bg-gray-900  flex justify-center '>
-        <div className="w-[80vw] max-w-md mx-auto h-[45vh] border border-gray-60 rounded-lg p-5 backdrop-blur-sm py-5 bg-white/30 flex justify-center xl:w-full xl:h-[80vh]">
-          <img src="https://images.unsplash.com/photo-1540427969750-1424f2fa0af8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aGFsbG93ZWVufGVufDB8fDB8fHww&auto=format&fit=crop&w=1000&q=60" className='rounded-2xl h-[40vh] xl:h-[75vh]' alt="" />
-        </div>
-        </div>
-        <div className='main bg-gray-900  flex justify-center  text-white text-center py-10'>
-          <div className='w-[80vw] font-serif text-sm xl:text-2xl '>
-          As the leaves turn crimson and the crisp breeze carries the scent of pumpkin spice, a certain enchantment takes hold of the world. It's that time of year when we welcome the mysterious, the eerie, and the supernatural with open arms. Spooky season, as it's affectionately known, is a time to revel in the thrill of all things macabre, a prelude to the grandeur of Halloween. In this guide, we'll delve into the essence of spooky season, exploring its history, traditions, and the myriad ways to fully embrace this bewitching time of year.
+        <div className='main bg-gray-900  flex justify-center flex-col text-center  text-white content-center '>
+          <div className='text-3xl py-10 text-white font-bold xl:text-4xl hover:text-purple-500'>Spooky Season is Here!</div>
+          <div className="w-[80vw] max-w-md mx-auto h-[45vh] border border-gray-60 rounded-lg p-5 backdrop-blur-sm py-5 bg-white/30 flex justify-center xl:w-full xl:h-[80vh]">
+            <img src="https://images.unsplash.com/photo-1540427969750-1424f2fa0af8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aGFsbG93ZWVufGVufDB8fDB8fHww&auto=format&fit=crop&w=1000&q=60" className='rounded-2xl h-[40vh] xl:h-[75vh]' alt="" />
+          </div>
+          <div className='flex justify-center p-16' >
+          <div className='w-[80vw] font-serif text-sm xl:text-2xl content-center text-center'>
+            As the leaves turn crimson and the crisp breeze carries the scent of pumpkin spice, a certain enchantment takes hold of the world. It's that time of year when we welcome the mysterious, the eerie, and the supernatural with open arms. Spooky season, as it's affectionately known, is a time to revel in the thrill of all things macabre, a prelude to the grandeur of Halloween. In this guide, we'll delve into the essence of spooky season, exploring its history, traditions, and the myriad ways to fully embrace this bewitching time of year.
+          </div>
+          </div>
+          <div className='flex justify-center xl:h-full pb-16'>
+          <img src="https://i.pinimg.com/originals/32/cd/0b/32cd0bb0462704dab050067adb1de597.gif" alt="" className='h-4/5 xl:w-[30vw]' />
+          {/* <img src="https://giffiles.alphacoders.com/158/15899.gif" alt="" className='w-4/5 xl:w-[30vw]'/> */}
           </div>
         </div>
-        <div className='main bg-gray-900  flex justify-center' >
-        <img src="https://i.pinimg.com/originals/32/cd/0b/32cd0bb0462704dab050067adb1de597.gif" alt="" className='w-4/5 xl:w-[30vw]'/>
-        {/* <img src="https://giffiles.alphacoders.com/158/15899.gif" alt="" className='w-4/5 xl:w-[30vw]'/> */}
-        </div>
-        <div className='main bg-gray-900  flex justify-center h-10 xl:h-16'>
-        </div>
+        
+
+        
+
+       
+       
+        
       </main>
     </>
   )
